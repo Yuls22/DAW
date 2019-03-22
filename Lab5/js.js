19318d4a2dcba07841891d3c3890595a21ba7cc5
Gilberto Pedraza <a01209124@itesm.mx>
2019-03-22 18:56:27 +0000

    Validador de password

---
+++ Lab5/js.js
@@ -0,0 +1,12 @@
+function validar(){
+    var usuario = document.getElementById("password").value ;
+    var password = document.getElementById("pass").value ;
+    
+    if(usuario == "yuls22" && password == "Ahri22.2"){
+        alert("Usuario datos correctos");
+    }
+    else{
+        alert("Verifique datos") ;
+    }
+    
+}
